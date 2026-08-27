a6e334be3eb94825f36d829255fbb257bc733180
emesekiniovo123 <emesekiniovo@gmail.com>
2026-08-27 16:41:54 +0100

    ok

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -13,21 +13,23 @@ const Contact = () => {
           service.
         </p>
       </div>
-      <div className="mx-auto w-full max-w-md rounded-[1.5rem] bg-green-600 p-6 text-white">
-        <div className="space-y-4 text-center">
-          <div>
-            <p className="text-sm text-white">Email</p>
-            <p className="font-medium">ng@onlineeatery.com</p>
-          </div>
-          <div>
-            <p className="text-sm text-white">Phone</p>
-            <p className="font-medium">+234 7012555687</p>
-          </div>
-          <div>
-            <p className="text-sm text-white">Address</p>
-            <p className="font-medium">
-              No. 3 Masaka, Karu LGA, Nasarawa State.{" "}
-            </p>
+      <div className="flex justify-center">
+        <div className="w-full max-w-md rounded-[1.5rem] bg-green-600 p-6 text-white">
+          <div className="space-y-4 text-center">
+            <div>
+              <p className="text-sm text-white">Email</p>
+              <p className="font-medium">ng@onlineeatery.com</p>
+            </div>
+            <div>
+              <p className="text-sm text-white">Phone</p>
+              <p className="font-medium">+234 7012555687</p>
+            </div>
+            <div>
+              <p className="text-sm text-white">Address</p>
+              <p className="font-medium">
+                No. 3 Masaka, Karu LGA, Nasarawa State.{" "}
+              </p>
+            </div>
           </div>
         </div>
       </div>
