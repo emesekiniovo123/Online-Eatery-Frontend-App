8ef53660fd8183d446533111e987b56ee0fb8620
emesekiniovo123 <emesekiniovo@gmail.com>
2026-08-27 16:27:12 +0100

    ok

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -13,19 +13,21 @@ const Contact = () => {
           service.
         </p>
       </div>
-      <div className="rounded-[1.5rem] bg-dark-900 p-6 text-white">
-        <div className="space-y-4">
+      <div className="mx-auto w-full max-w-md rounded-[1.5rem] bg-green-600 p-6 text-white">
+        <div className="space-y-4 text-center">
           <div>
-            <p className="text-sm text-dark-400">Email</p>
+            <p className="text-sm text-white">Email</p>
             <p className="font-medium">ng@onlineeatery.com</p>
           </div>
           <div>
-            <p className="text-sm text-dark-400">Phone</p>
+            <p className="text-sm text-white">Phone</p>
             <p className="font-medium">+234 7012555687</p>
           </div>
           <div>
-            <p className="text-sm text-dark-400">Address</p>
-            <p className="font-medium">No. 3 Masaka, Karu LGA, Nasarawa State. </p>
+            <p className="text-sm text-white">Address</p>
+            <p className="font-medium">
+              No. 3 Masaka, Karu LGA, Nasarawa State.{" "}
+            </p>
           </div>
         </div>
       </div>
